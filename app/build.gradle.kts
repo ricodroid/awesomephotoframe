52fbdd836966959d2f7414bb5305e489406fe369
ricodroid404 <ricodroid2024@gmail.com>
2025-03-13 19:32:58 -0400

    依存関係を参照するようにする

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,19 +55,15 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
-    implementation("com.github.bumptech.glide:glide:4.16.0") // 最新バージョンに適宜更新
-    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0") // Glideのコンパイラ
-    // ViewModel & LiveData (Lifecycle)
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
-    // Kotlin Coroutines (非同期処理を扱う場合)
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    // Gsonコンバーター (JSON を Kotlin オブジェクトに変換)
+    implementation(libs.glide)
+    annotationProcessor(libs.compiler)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.retrofit)
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.okhttp)
 
     // AdMob (Google 広告 SDK)
     implementation("com.google.android.gms:play-services-ads:24.1.0")
